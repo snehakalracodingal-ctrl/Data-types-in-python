01ecd1491d16b5ab7c1c75dab7ef321475980fbc
snehakalracodingal-ctrl <snehakalra.codingal@gmail.com>
2026-01-06 19:12:37 +0530

    activity2

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+
+name = "Sneha Kalra"
+age = 24
+is_teacher = True
+weight = 60
+
+# Printing Different Variables and their Data Type
+print("Name :", name)
+print("Data Type of Name is", type(name))
+
+print("Age :", age)
+print("Data Type of Age is", type(age))
+
+print("is_teacher :", is_teacher)
+print("Data Type of is_teacher is", type(is_teacher))
+
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+
